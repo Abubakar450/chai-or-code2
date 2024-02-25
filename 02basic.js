838a4d36e656311f7000037b3c9f1fa4f19e2571
Abubakar450 <140898222+Abubakar450@users.noreply.github.com>
2024-02-25 08:09:02 +0000

    data types

---
+++ 02basic.js
@@ -0,0 +1,11 @@
+//data tyypes
+/*
+primative data type
+1 number =2 to power 53
+2 string= ""
+3 bollen=true or false
+4 null =type of object*
+5 undifind=type of undefind
+6 nan =not a number
+7 symbol =unique
+*/
