7695fe148cdfa1a5d1fac41e60d444199519765e
Abubakar450 <140898222+Abubakar450@users.noreply.github.com>
2024-02-28 08:35:59 -0800

    Create 03basic.js

---
+++ 03basic.js
@@ -0,0 +1,14 @@
+/*let secore =44
+console.log(secore);
+toString=String(secore)
+console.log(typeof secore);
+console.log(typeof toString);*/
+/*let str="name"
+console.log(typeof str);
+tonumber=Number(str)
+console.log(tonumber);*/
+let islogedIn=0
+console.log( typeof islogedIn);
+toblean=Boolean(islogedIn)
+console.log(toblean);
+console.log(typeof toblean);
